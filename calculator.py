8c08bd9530ff7a75e54a29ba68eccf0844c6b945
Darryl Lane <darryllane101@gmail.com>
2019-03-30 22:30:30 +0000

    update

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import time
+
 # This function adds two numbers 
 def add(x, y):
 	return x + y
@@ -15,38 +17,44 @@ def divide(x, y):
 	return x / y
 
 def main():
-	__version__ = '0.1'
-	print("Select operation.")
-	print("1.Add")
-	print("2.Subtract")
-	print("3.Multiply")
-	print("4.Divide")
-	print("5.Version")
-	
-	# Take input from the user 
-	choice = int(input("Enter choice(1/2/3/4):"))
-	
-	num1 = int(str(input("Enter first number: ")).replace(' ', ''))
-	num2 = int(str(input("Enter second number: ")).replace(' ', ''))
-	
-	if choice == 1:
-		print('{} + {} = {}'.format(num1, num2, add(num1,num2)))
-		return add(num1,num2)
-	
-	elif choice == 2:
-		print('{} - {} = {}'.format(num1, num2, subtract(num1,num2)))
-		return subtract(num1,num2)
-	
-	elif choice == 3:
-		print('{} x {} = {}'.format(num1, num2, multiply(num1,num2)))
-		return multiply(num1,num2)
-	
-	elif choice == 4:
-		print('{} / {} = {}'.format(num1, num2, divide(num1,num2)))
-		return divide(num1,num2)
-	
-	elif choice == 5:
-		print(__version__)
+	__version__ = '0.1.1'
+	while True:
+		print("\nSelect Operator")
+		print("1.Add")
+		print("2.Subtract")
+		print("3.Multiply")
+		print("4.Divide")
+		print("5.Version")
 		
-	else:
-		print("Invalid input")
+		# Take input from the user 
+		
+		choice = int(input("Enter choice: "))
+		
+		if choice == 5:
+			print('\nVersion: {}'.format(__version__))
+		else:
+			
+			num1 = int(str(input("\nEnter first number: ")).replace(' ', ''))
+			num2 = int(str(input("Enter second number: ")).replace(' ', ''))
+		
+		
+			if choice == 1:
+				print('{} + {} = {}'.format(num1, num2, add(num1,num2)))
+				return add(num1,num2)
+			
+			elif choice == 2:
+				print('{} - {} = {}'.format(num1, num2, subtract(num1,num2)))
+				return subtract(num1,num2)
+			
+			elif choice == 3:
+				print('{} x {} = {}'.format(num1, num2, multiply(num1,num2)))
+				return multiply(num1,num2)
+			
+			elif choice == 4:
+				print('{} / {} = {}'.format(num1, num2, divide(num1,num2)))
+				return divide(num1,num2)
+			else:
+				print("Invalid Input")
+			
+if __name__ == "__main__":
+	main()
